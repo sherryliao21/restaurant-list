7e3878f9af1d2d35e7c8eb0111044a8c318fd3ae
Sherry <sherry.c.liao@gmail.com>
2021-01-25 16:10:54 +0800

    feat: modify router.js using object destructuring style

--- routes/router.js
+++ routes/router.js
@@ -43,17 +43,7 @@ router.get('/search', (req, res) => {
     .catch(error => console.log(error))
 })
 router.post('/restaurants', (req, res) => {
-  const restaurantItem = req.body
-  let { name, category, image, location, phone, rating, description } = restaurant
-  restaurant = {
-    name: restaurantItem.name,
-    category: restaurantItem.category,
-    image: restaurantItem.image,
-    location: restaurantItem.location,
-    phone: restaurantItem.phone,
-    rating: restaurantItem.rating,
-    description: restaurantItem.description
-  }
+  const { name, category, image, location, phone, rating, description } = req.body
   return Restaurant.create({ name, category, image, location, phone, rating, description })
     .then(() => res.redirect('/'))
     .catch(error => console.log(error))
@@ -61,22 +51,10 @@ router.post('/restaurants', (req, res) => {
 
 router.post('/restaurants/:restaurant_id/edit', (req, res) => {
   const id = req.params.restaurant_id
-  const name = req.body.name
-  const category = req.body.category
-  const image = req.body.image
-  const location = req.body.location
-  const phone = req.body.phone
-  const rating = req.body.rating
-  const description = req.body.description
+  const { name, category, image, location, phone, rating, description } = req.body
   return Restaurant.findById(id)
     .then(restaurant => {
-      restaurant.name = name
-      restaurant.category = category
-      restaurant.image = image
-      restaurant.location = location
-      restaurant.phone = phone
-      restaurant.rating = rating
-      restaurant.description = description
+      const { name, category, image, location, phone, rating, description } = restaurant
       return restaurant.save()
     })
     .then(() => res.redirect(`/restaurants/${id}`))
