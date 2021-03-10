13f3e17551c9c795227cc4690c0130000dc3c0c5
Sherry <sherry.c.liao@gmail.com>
2021-03-11 00:18:12 +0800

    feat: add bcrypt to login route

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -21,7 +21,7 @@ router.get('/register', (req, res) => {
 router.post('/register', (req, res) => {
   const { name, email, password, confirmPassword } = req.body
   const errors = []
-  if (!name || !email || !password || !confirmPassword) {
+  if (!email || !password || !confirmPassword) {
     errors.push({ message: '所有欄位都是必填。' })
   }
   if (name && email && password && confirmPassword) {
